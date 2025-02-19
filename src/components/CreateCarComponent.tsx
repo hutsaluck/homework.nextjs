5be577b785ecb62d282ecd476614001fd505da7d
Юра Гуцалюк <hutsaluck@gmail.com>
2025-02-19 16:31:47 +0200

    Create project

--- src/components/CreateCarComponent.tsx
+++ src/components/CreateCarComponent.tsx
@@ -1,50 +1,81 @@
+'use client'
+
+import {useForm} from "react-hook-form";
+import {ICar} from "@/models/ICar";
+import {joiResolver} from "@hookform/resolvers/joi";
+import {CarValidator} from "@/validators/CarValidator";
+import {useState} from "react";
 import {saveCar} from "@/server-actions/serverActions";
 
 export const CreateCarComponent = () => {
+    const {register, handleSubmit, reset, formState: {errors, isValid}} = useForm<ICar>({
+        mode: 'all',
+        resolver: joiResolver(CarValidator)
+    })
+
+    const [message, setMessage] = useState<boolean>(false)
+
+    const createHandler = async (data: ICar) => {
+        await saveCar(data)
+        setMessage(true)
+        reset()
+        setTimeout(() => {
+            setMessage(false)
+        }, 3000)
+    }
+
     return (
         <div className="max-w-2xl mx-auto">
             <h2 className="text-2xl font-bold text-center mb-6">Do you want to build your own car?</h2>
             <p className="text-center text-lg mb-6">Just do it!</p>
-            <form className="grid gap-5" action={saveCar}>
+            <form className="grid gap-5" onSubmit={handleSubmit(createHandler)}>
                 <label htmlFor="brand" className="relative">
-                    <p className="bg-white py-0 font-light absolute px-1 -top-3 left-4 bg-primary-color text-gray-500">Brand</p>
+                    <p className="bg-white py-0 font-light absolute px-1 -top-3 left-4 bg-primary-color text-gray-500">Name</p>
                     <input
                         type="text"
-                        name={'brand'}
+                        {...register('brand')}
                         id="brand"
                         placeholder="Brand"
                         className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 text-gray-500"
                     />
+                    {errors.brand && <p className="text-red-500 text-sm mt-2">{errors.brand.message}</p>}
                 </label>
 
                 <label htmlFor="price" className="relative">
                     <p className="bg-white py-0 font-light absolute px-1 -top-3 left-4 bg-primary-color text-gray-500">Price</p>
                     <input
                         type="number"
-                        name={'price'}
+                        {...register('price')}
                         id="price"
                         placeholder="Price"
                         className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 text-gray-500"
                     />
+                    {errors.price && <p className="text-red-500 text-sm mt-2">{errors.price.message}</p>}
                 </label>
 
                 <label htmlFor="year" className="relative">
                     <p className="bg-white py-0 font-light absolute px-1 -top-3 left-4 bg-primary-color text-gray-500">Year</p>
                     <input
                         type="year"
-                        name={'year'}
+                        {...register('year')}
                         id="year"
                         placeholder="Year"
                         className="w-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500 text-gray-500"
                     />
+                    {errors.year && <p className="text-red-500 text-sm mt-2">{errors.year.message}</p>}
                 </label>
 
                 <input
                     type="submit"
                     value="Create car"
-                    className="px-10 py-4 uppercase bg-gray-300 rounded-lg cursor-pointer hover:bg-gray-400 text-gray-500"
+                    disabled={!isValid}
+                    className="text-gray-500 px-10 py-4 uppercase bg-gray-300 rounded-lg cursor-pointer hover:bg-gray-400 disabled:cursor-not-allowed disabled:bg-gray-200"
                 />
             </form>
+            {message &&
+                <p className="text-center text-sm mt-4 text-gray-500">
+                    Car was created!
+                </p>}
         </div>
 
     );
--- src/server-actions/serverActions.tsx
+++ src/server-actions/serverActions.tsx
@@ -4,12 +4,7 @@ import {addCar} from "@/services/api.service";
 import {ICar} from "@/models/ICar";
 import {redirect} from "next/navigation";
 
-export const saveCar = async (formData: FormData) => {
-    const car:ICar = {
-        brand: String(formData.get('brand')),
-        price: Number(formData.get('price')),
-        year: Number(formData.get('year')),
-    };
+export const saveCar = async (car: ICar) => {
     await addCar(car)
 
     redirect('/cars')
